65103ad7246c2f6465c16c1d7acd2045bce79da2
Benedict Wong <bwa44@sfu.ca>
2021-09-09 15:20:08 -0700

    changed aspect ratio to 1080x1920

--- Assets/Scripts/Misc/LoseZone.cs
+++ Assets/Scripts/Misc/LoseZone.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class LoseZone : MonoBehaviour
 {
     // Start is called before the first frame update
-    private void OnTriggerExit2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
 	    if (other.CompareTag("Monkey Body")) {
 		    PlayerPrefs.SetString("winLoseCondition", "lose");
 		    SceneManager.LoadScene("Scenes/EndScene");
